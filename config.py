d42bd9d17f44f613a27768c9cadeb59005c8650b
tyler <tyler_d1128@outlook.com>
2020-12-02 19:37:29 +0800

    feat: add config.py

---
+++ config.py
@@ -0,0 +1,32 @@
+# _*_ coding: utf-8 _*_
+"""
+@Author     : tyler
+@Email      : tyler_d1128@outlook.com
+@Project    : bilibili-video-download 
+@File       : config.py
+@License    : GNU GENERAL PUBLIC LICENSE
+"""
+from requests import session
+from bilibili_api import Verify
+
+config = {
+    'BILIBILI': {
+        'SESSDATA': '',
+        'CSRF': '',
+    },
+    'DATA_PATH': '',
+}
+
+verify = Verify(sessdata=config['BILIBILI']['SESSDATA'], csrf=config['BILIBILI']['CSRF'])
+
+
+def get_session():
+    sessions = session()
+    sessions.headers.update({
+        'Referer': 'https://www.bilibili.com',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_0_0) AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/86.0.4240.198 Safari/537.36',
+        'Origin': 'https://www.bilibili.com',
+    })
+    return sessions
+
